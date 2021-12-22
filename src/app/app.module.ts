0703b9d8da35b933b0bc33e4f60a1b1c7a5b1792
vutienminh1919 <vutineminh1919@gmail.com>
2021-12-22 08:49:07 +0700

    bai thuc hanh form-angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { TodoComponent } from './component/todo/todo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
